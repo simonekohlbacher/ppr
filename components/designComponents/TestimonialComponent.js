5de93b37407456ef78942b6d26495b0b839eb104
AnschiPeer <s2210456025@fhooe.at>
2025-04-28 10:35:47 +0200

    bilder

--- components/designComponents/TestimonialComponent.js
+++ components/designComponents/TestimonialComponent.js
@@ -13,14 +13,14 @@ export default class TestimonialComponent extends KWM_Component {
               <div class="realtive flex items-center">
                 <!-- Logo & Foto -->
                 <div>
-                    <div class="bg-[#F8EC19] w-80 h-80 rounded-full">
-                        <div></div>   
-                   
+                    <div class="bg-[var(--bzl-yellow)] w-80 h-80 rounded-full">
+                        <div><img src="../../img/Bernhard-Lintner.png" alt="Portrait Bernhard Lintner"></div> 
                     </div>
+                    <div><img src="../../img/lenzing-logo.png" alt="Lenzing Logo"> </div>
                 </div>
                 
                 <!-- Text -->
-                <div>
+                <div class="bg-[var(--bzl-light-grey)]">
                    
                 </div>
                 
