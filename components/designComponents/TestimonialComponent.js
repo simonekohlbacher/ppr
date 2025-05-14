0afa5614accff4e8be71247285c31cb317b48887
AnschiPeer <s2210456025@fhooe.at>
2025-05-14 12:15:07 +0200

    SVG adapted

--- components/designComponents/TestimonialComponent.js
+++ components/designComponents/TestimonialComponent.js
@@ -16,10 +16,9 @@ export default class TestimonialComponent extends KWM_Component {
           <section class="testimonial py-10 relative overflow-hidden mt-20 mb-20">
             
             <div class="hidden lg:block absolute top-0 bottom-0 right-0 z-0 pointer-events-none" style="width: 80%;">
-                <svg viewBox="0 0 1506 600" xmlns="http://www.w3.org/2000/svg" class="h-full">
-                    <path d="M165.495 600H1149V0H165.495C-137.841 273.472 45.8979 526.672 165.495 600Z" fill="#E8E8E8"/>
-                    <!-- Rechteck auf die gesamte Breite des Viewports setzen -->
-                    <rect x="1147" width="100vw" height="100%" fill="#E8E8E8"/>
+                <svg viewBox="0 0 2000 600" xmlns="http://www.w3.org/2000/svg" class="h-full">
+                    <path d="M165.495 600H1799V0H165.495C-137.841 273.472 45.8979 526.672 165.495 600Z" fill="#E8E8E8"/>                       
+                    <rect x="1799" width="100vw" height="100%" fill="#E8E8E8"/>
                 </svg>
             </div>
             <div class="relative ">
